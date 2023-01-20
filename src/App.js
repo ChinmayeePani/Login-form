b1d73bbdd0417235010d68c15eb5c45ec41eb786
ChinmayeePani <cpani1997@gmail.com>
2023-01-20 11:17:43 +0530

    Login-form created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import profile from "./Image/dp.jpg";
+import email from "./Image/email.png";
+import password from "./Image/password.png";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="sub-main">
+        <div className="image">
+          
+            <img src={profile} alt="profile" className="profile" />
+          
+        </div>
+        
+          <h1>LOGIN PAGE</h1>
+        
+        <div>
+          <img src={email} alt="email" className="email" />
+          <input type="text" placeholder="email" className="name" />
+          </div>
+
+        <div className="second">
+          <img src={password} alt="password" className="email" />
+          <input type="password" placeholder="password" className="name" />
+        </div>
+
+        <div className="login-button">
+        <button>Login</button></div>
+        <p className="hyper"> 
+      <a href="#">Forget Password ?</a> Or <a href="#">Sign Up</a>
+      </p>
+      </div>
+      
     </div>
   );
 }
